515b87a94bd819516b101000408c4d543626e534
gzrizk <gonzalorizk@gmail.com>
2025-04-29 18:11:22 -0300

    genere coordenadas para cada individuo

---
+++ script.R
@@ -0,0 +1,17 @@
+print ("Hello World")
+
+a <-1
+
+
+example_data <- data.frame(x = 1:10, y = 1:10, z = 1:10)
+print(example_data)
+
+hist(example_data
+$y, main = "Histogram of y", xlab = "y values", col = "blue", border = "black")
+
+
+
+
+
+
+
