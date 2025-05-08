cff89981beba2a0d5868437ba59b4e3439f8fd8b
gzrizk <gonzalorizk@gmail.com>
2025-05-08 19:00:10 -0300

    readmarkups intento

--- visualization.R
+++ visualization.R
@@ -5,4 +5,12 @@ library(here)
 mesh <-read.ply("data/3D/BH_PG_14NOG1.ply", ShowSpecimen = TRUE)
 
 
+library(SlicerMorphR)
 
+lmks<- read.markups.json ("samplelm.mrk.json")
+
+#ademas de leerlo necesito armar un array, donde las dimensiones van a ser:
+#numero de landmarks, 3 (por x,y,z) y F siendo F el numero de archivos. 
+
+# myLMs = array (dim = c(53,3,62))
+# for (i in 1:62)  myLMs [,,i] = read.markups.json (files[i)])  # i read each file
